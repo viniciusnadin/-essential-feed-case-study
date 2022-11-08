78ad96497153301c806d73ed4902ae3df6fd0bd7
Vinicius Nadin <vininadin@gmail.com>
2022-11-08 09:28:37 -0300

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vinicius Nadin on 08/11/22.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
