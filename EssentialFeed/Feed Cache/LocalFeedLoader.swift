8cbcd1be7cd1cff6856f15f2e0d096f82f289c61
Vinicius Nadin <vininadin@gmail.com>
2022-11-10 21:38:38 -0300

    Extract cache deletion side-effects on expired cache from the `load` method to the `validateCache` method

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -41,7 +41,6 @@ public final class LocalFeedLoader {
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
             case .found:
-                self.store.deleteCacheFeed { _ in }
                 completion(.success([]))
             case .empty:
                 completion(.success([]))
@@ -56,10 +55,11 @@ public final class LocalFeedLoader {
     public func validateCache() {
         store.retrieve { [unowned self] result in
             switch result {
+            case let .found(_, timestamp) where !self.validate(timestamp):
+                self.store.deleteCacheFeed { _ in }
             case .failure:
                 self.store.deleteCacheFeed { _ in }
-            default:
-                break
+            case .empty, .found: break
             }
         }
     }
