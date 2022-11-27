ef7aa79e580730e6554cfd06691b9727ab55670c
Vinicius Nadin <vininadin@gmail.com>
2022-11-27 17:25:44 -0300

    Add empty project for prototype

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Vinicius Nadin on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
