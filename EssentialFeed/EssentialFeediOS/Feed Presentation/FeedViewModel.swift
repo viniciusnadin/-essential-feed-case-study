15379879e34f1e38bb2a1d0202ed528a88c3ccc0
Vinicius Nadin <vininadin@gmail.com>
2022-12-15 10:22:38 -0300

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Vinicius Nadin on 15/12/22.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
