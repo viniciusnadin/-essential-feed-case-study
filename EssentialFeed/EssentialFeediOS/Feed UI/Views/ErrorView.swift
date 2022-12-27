f97c3f61e72543f6fafd73873166f171f5bcee87
Vinicius Nadin <vininadin@gmail.com>
2022-12-27 09:37:12 -0300

    Show and Hide error message animated

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -16,8 +16,8 @@ public final class ErrorView: UIView {
     }()
     
     public var message: String? {
-        get { return errorButton.title(for: .normal) }
-        set { errorButton.setTitle(newValue, for: .normal) }
+        get { return isVisible ? errorButton.title(for: .normal) : nil }
+        set { setMessageAnimated(newValue) }
     }
     
     public override init(frame: CGRect) {
@@ -25,6 +25,7 @@ public final class ErrorView: UIView {
         message = nil
         makeErrorButtonConstraints()
         self.backgroundColor = .red
+        alpha = 0
     }
     
     required init?(coder: NSCoder) {
@@ -39,5 +40,34 @@ public final class ErrorView: UIView {
             errorButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             errorButton.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
+    }
+    
+    private var isVisible: Bool {
+        return alpha > 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message = message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        errorButton.setTitle(message, for: .normal)
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0 },
+            completion: { completed in
+                if completed { self.errorButton.setTitle(nil, for: .normal) }
+            })
     }
 }
