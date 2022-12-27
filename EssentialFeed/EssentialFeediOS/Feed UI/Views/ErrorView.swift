b75d834a282e2ae881686af5bb76819dd7444837
Vinicius Nadin <vininadin@gmail.com>
2022-12-27 09:33:32 -0300

    Configure errorView layout

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -8,5 +8,36 @@
 import UIKit
 
 public final class ErrorView: UIView {
-    public var message: String?
+    
+    private lazy var errorButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    public var message: String? {
+        get { return errorButton.title(for: .normal) }
+        set { errorButton.setTitle(newValue, for: .normal) }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        message = nil
+        makeErrorButtonConstraints()
+        self.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func makeErrorButtonConstraints() {
+        self.addSubview(errorButton)
+        NSLayoutConstraint.activate([
+            errorButton.topAnchor.constraint(equalTo: self.topAnchor),
+            errorButton.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            errorButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            errorButton.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
 }
