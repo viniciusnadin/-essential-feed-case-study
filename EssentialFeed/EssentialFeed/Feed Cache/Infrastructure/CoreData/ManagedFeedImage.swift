46c757f2f3aa1babd7f14ea1e018274d46143a23
Vinicius Nadin <vininadin@gmail.com>
2023-01-05 10:56:10 -0300

    `CoreDataFeedStore.retrieveImageData` delivers stored data when there's an image with a matching URL in the store

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,7 +8,7 @@
 import CoreData
 
 @objc(ManagedFeedImage)
- class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged  var id: UUID
     @NSManaged  var imageDescription: String?
     @NSManaged  var location: String?
@@ -18,7 +18,15 @@ import CoreData
 }
 
 extension ManagedFeedImage {
-     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
+        let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+        request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+        request.returnsObjectsAsFaults = false
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
@@ -28,8 +36,8 @@ extension ManagedFeedImage {
             return managed
         })
     }
-
-     var local: LocalFeedImage {
+    
+    var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
 }
