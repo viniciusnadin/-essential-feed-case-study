9ff47e1058adbb70af0d5bd906770c62772dcdc8
Vinicius Nadin <vininadin@gmail.com>
2023-02-20 14:58:49 -0300

    Add `RemoteWithLocalFallbackFeedLoaderTests` tests

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteWithLocalFallbackFeedLoaderTests.swift
+//  EssentialAppTests
+//
+//  Created by Vinicius Nadin on 20/02/23.
+//
+
+import XCTest
+
+class RemoteWithLocalFallbackFeedLoaderTests: XCTestCase {
+    
+}
