4d7b34a89b91bd8267f8c90b498e3356de5af010
Vinicius Nadin <vininadin@gmail.com>
2023-05-27 23:40:20 -0300

    Fix all wrong tests

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -13,10 +13,13 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
+            MainQueueDispatchDecorator(decoratee: feedLoader))
         
-        let feedController = makeFeedViewController(delegate: presentationAdapter, errorView: ErrorView(), title: FeedPresenter.title)
-        
+        let feedController = makeFeedViewController(
+            delegate: presentationAdapter,
+            title: FeedPresenter.title)
+
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
                 controller: feedController,
@@ -26,11 +29,12 @@ public final class FeedUIComposer {
         
         return feedController
     }
-    
-    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, errorView: ErrorView, title: String) -> FeedViewController {
-        let feedController = FeedViewController()
+
+    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
         feedController.delegate = delegate
-        feedController.errorView = errorView
         feedController.title = title
         return feedController
     }
--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -8,39 +8,18 @@
 import UIKit
 
 public final class ErrorView: UIView {
-    
-    private lazy var errorButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
-        return button
-    }()
-    
+    @IBOutlet private var label: UILabel!
+
     public var message: String? {
-        get { return isVisible ? errorButton.title(for: .normal) : nil }
+        get { return isVisible ? label.text : nil }
         set { setMessageAnimated(newValue) }
     }
     
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        message = nil
-        makeErrorButtonConstraints()
-        self.backgroundColor = .red
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        label.text = nil
         alpha = 0
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func makeErrorButtonConstraints() {
-        self.addSubview(errorButton)
-        NSLayoutConstraint.activate([
-            errorButton.topAnchor.constraint(equalTo: self.topAnchor),
-            errorButton.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            errorButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            errorButton.trailingAnchor.constraint(equalTo: self.trailingAnchor)
-        ])
     }
     
     private var isVisible: Bool {
@@ -54,21 +33,21 @@ public final class ErrorView: UIView {
             hideMessageAnimated()
         }
     }
-    
+
     private func showAnimated(_ message: String) {
-        errorButton.setTitle(message, for: .normal)
+        label.text = message
         
         UIView.animate(withDuration: 0.25) {
             self.alpha = 1
         }
     }
     
-    @objc private func hideMessageAnimated() {
+    @IBAction private func hideMessageAnimated() {
         UIView.animate(
             withDuration: 0.25,
             animations: { self.alpha = 0 },
             completion: { completed in
-                if completed { self.errorButton.setTitle(nil, for: .normal) }
+                if completed { self.label.text = nil }
             })
     }
 }
