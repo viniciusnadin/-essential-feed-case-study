e16d33df1580b801c8ff84c9d0cb5a6da56d03b8
Vinicius Nadin <vininadin@gmail.com>
2023-05-29 17:52:18 -0300

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -34,8 +34,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
         configureWindow()
     }
     
