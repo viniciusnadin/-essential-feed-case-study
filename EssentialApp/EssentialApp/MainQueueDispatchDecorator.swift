1323caffb8c319cd41cf8fdde5557709b5ac37e9
Vinicius Nadin <vininadin@gmail.com>
2023-05-30 15:31:44 -0300

    Replace FeedLoader composition with Combine operators

--- EssentialApp/EssentialApp/MainQueueDispatchDecorator.swift
+++ EssentialApp/EssentialApp/MainQueueDispatchDecorator.swift
@@ -10,25 +10,17 @@ import EssentialFeed
 
 final class MainQueueDispatchDecorator<T> {
     private let decoratee: T
-
+    
     init(decoratee: T) {
         self.decoratee = decoratee
     }
-
+    
     func dispatch(completion: @escaping () -> Void) {
         guard Thread.isMainThread else {
             return DispatchQueue.main.async(execute: completion)
         }
-
+        
         completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
     }
 }
 
