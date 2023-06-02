ec33a9760b3e968f6d185a677cc59719c51ccd8f
Vinicius Nadin <vininadin@gmail.com>
2023-06-02 18:51:54 -0300

    Add image comments title

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCommentsPresenter.swift
+//  EssentialFeed
+//
+//  Created by Vinicius Nadin on 02/06/23.
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view")
+    }
+}
