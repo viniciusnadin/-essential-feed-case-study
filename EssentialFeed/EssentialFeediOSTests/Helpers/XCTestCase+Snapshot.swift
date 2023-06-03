c46ae70d7ef123dd323d6dd3a1d146beb6a57d81
Vinicius Nadin <vininadin@gmail.com>
2023-06-03 19:36:25 -0300

    Add XCTFail to remind us to call assert after recording a snapshot

--- EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -40,7 +40,7 @@ extension XCTestCase {
 
             try snapshotData?.write(to: snapshotURL)
         } catch {
-            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+            XCTFail("Record succeeded - use `assert` to compare the snapshot from now on.", file: file, line: line)
         }
     }
 
